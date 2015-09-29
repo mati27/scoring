db843fcf5eeb6e3e5a2035ddf81efdef2d6a5e08
Matias Dinota <mdinota@eryxsoluciones.com.ar>
2015-09-29 19:09:35 -0300

    Se puede configurar el tiempo de actualizacion del GPS

--- tests/gps.py
+++ tests/gps.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from datetime import timedelta
 from geolocalizacion.gps import GPS
 from geolocalizacion.satelite import SateliteMock, SimuladorDeRecorrido, RecorridoEnLista
 
@@ -18,6 +19,9 @@ class GPSTestCase(TestCase):
     def setUp(self):
         self.ubicaciones_obtenidas = []
 
+    def un_gps(self, satelite):
+        return GPS.nuevo(satelite=satelite, actualizar_cada=timedelta(microseconds=1))
+
     def test_al_obtener_la_ubicacion_actual_el_GPS_obtiene_la_ubicacion_del_satelite(self):
         satelite = SateliteSpy()
         gps = self.un_gps(satelite=satelite)
@@ -25,9 +29,6 @@ class GPSTestCase(TestCase):
         gps.obtener_ubicacion_actual()
 
         self.assertTrue(satelite.ubicacion_fue_obtenida_desde(gps))
-
-    def un_gps(self, satelite):
-        return GPS.nuevo(satelite=satelite)
 
     def test_al_activar_el_gps_los_que_estan_escuchando_reciben_las_coordenadas_obtenidas_del_satelite(self):
         estrategia_de_recorrido = RecorridoEnLista([(-34.551212, -58.463000), (-34.551882, -58.462591)])
