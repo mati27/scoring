5274588475169a464e2e75910800e911155a0ed1
Matias Dinota <mdinota@eryxsoluciones.com.ar>
2015-10-01 01:46:24 -0300

    Implementacion del detector de viaje a zona peligrosa
    - Agregué la clase abstracta Evento y el EventoDeViajeAZonaPeligrosa.
    - Implementé una primera versión de ZonaGeografica que solo puede ser un rectangulo.
    - Implementé el detector con sus tests.

---
+++ eventos/zona_peligrosa.py
@@ -0,0 +1,15 @@
+from eventos.base import Evento
+
+
+class EventoDeViajeAZonaPeligrosa(Evento):
+    @classmethod
+    def nuevo(cls, zona):
+        return cls(zona=zona)
+
+    def __init__(self, zona):
+        self._zona = zona
+
+        super(EventoDeViajeAZonaPeligrosa, self).__init__()
+
+    def zona(self):
+        return self._zona
