b45f58bcaa27154cbebb6535e04afd06b544a15c
b3rnix <bpanarello@gmail.com>
2015-10-03 19:16:08 -0300

    Scoreador, cotizadores

---
+++ scoreador/proveedor_cotizadores.py
@@ -0,0 +1,6 @@
+class ProveedorCotizadores:
+    def obtener_cotizadores_eventos(self):
+        raise NotImplementedError('responsabilidad de la subclase')
+
+    def obtener_cotizador_kilometraje(self):
+        raise NotImplementedError('responsabilidad de la subclase')
