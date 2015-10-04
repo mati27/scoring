375b504ca3b2259a89e56789b8c0997afd80ccb4
cyntia <cyntiab83@gmail.com>
2015-10-04 09:24:51 -0300

    Evento exceso de velocidad

---
+++ eventos/exceso_velocidad.py
@@ -0,0 +1,19 @@
+from eventos.base import Evento
+
+
+class EventoDeExcesoDeVelocidad(Evento):
+    @classmethod
+    def nuevo(cls, porcentaje_de_velocidad, velocidad_excedido):
+        return cls(porcentaje_de_velocidad=porcentaje_de_velocidad, velocidad_excedido=velocidad_excedido)
+
+    def __init__(self, porcentaje_de_velocidad, velocidad_excedido):
+        self._porcentaje_de_velocidad = porcentaje_de_velocidad
+        self._velocidad_excedido = velocidad_excedido
+
+        super(EventoDeExcesoDeVelocidad, self).__init__()
+
+    def porcentaje_de_velocidad(self):
+        return self._porcentaje_de_velocidad
+
+    def velocidad_excedido(self):
+        return self._velocidad_excedido
